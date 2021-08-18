747e51b77e5fc91a26a58f8419288b3ec014a55d
Gurkirpal <gurkirpal204@gmail.com>
2021-08-18 19:03:02 +0530

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Getting Started with Create React App
+# Giava
+Giava is another video app
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [hasPermissions, setHasPermission] = useState();
+  const [isUsingVideo, setIsUsingVideo] = useState(false);
+  const [currentStream, setCurrentStream] = useState();
+  const playerRef = useRef(null);
+
+  const handleStartVideo = () => {
+    setIsUsingVideo(true);
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    }).then(stream => {
+      console.log('got stream', stream);
+      setCurrentStream(stream);
+      setHasPermission(true);
+    }).catch(error => {
+      console.log('Media access error', error);
+      setHasPermission(false);
+    });
+  };
+
+  const handleStopVideo = () => {
+    setIsUsingVideo(false);
+    currentStream.getTracks().forEach(track => {
+      if (track.readyState === 'live') {
+        track.stop();
+      }
+    });
+    setCurrentStream(null);
+  };
+
+  useEffect(() => {
+    if (playerRef.current && !playerRef.current.srcObject && isUsingVideo && currentStream) {
+      playerRef.current.srcObject = currentStream;
+    }
+  }, [currentStream, isUsingVideo, playerRef]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {
+          isUsingVideo ?
+            <>
+              {
+                currentStream &&
+                <button onClick={handleStopVideo}>Stop video</button>
+              }
+            </> :
+            <button onClick={handleStartVideo}>Start video</button>
+        }
+
+
+      </div>
+      {
+        isUsingVideo &&
+        <>
+          {(typeof hasPermissions === 'boolean') ?
+            <div>
+              {
+                hasPermissions ?
+                  'Audio and video is ready' :
+                  'App will require audio and video permissions to function properly'
+              }
+            </div> :
+            <div>
+              Starting...
+            </div>
+          }
+        </>
+      }
+
+      {isUsingVideo && currentStream &&
+        <div>
+          Video:
+          <video id="localVideo" autoPlay playsInline ref={playerRef} style={{ transform: 'scaleX(-1)' }} />
+        </div>
+      }
     </div>
   );
 }
