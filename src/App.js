9e54b28700e9de7174d566d774f58fa839dca5af
Gurkirpal <gurkirpal204@gmail.com>
2021-08-20 01:05:27 +0530

    Add mute/unmute
    Camera light shows on even when video is muted

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [hasPermissions, setHasPermission] = useState();
   const [isUsingVideo, setIsUsingVideo] = useState(false);
   const [currentStream, setCurrentStream] = useState();
+  const [isMuted, setIsMuted] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
   const playerRef = useRef(null);
 
   const handleStartVideo = () => {
@@ -30,6 +32,42 @@ function App() {
       }
     });
     setCurrentStream(null);
+  };
+
+  const handleMute = () => {
+    const tracks = currentStream.getAudioTracks();
+    if (tracks.length > 0) {
+      const track = tracks[0];
+      track.enabled = false;
+      setIsMuted(true);
+    }
+  };
+
+  const handleDisableVideo = () => {
+    const tracks = currentStream.getVideoTracks();
+    if (tracks.length > 0) {
+      const track = tracks[0];
+      track.enabled = false;
+      setIsVideoMuted(true);
+    }
+  };
+
+  const handleUnMute = () => {
+    const tracks = currentStream.getAudioTracks();
+    if (tracks.length > 0) {
+      const track = tracks[0];
+      track.enabled = true;
+      setIsMuted(false);
+    }
+  };
+
+  const handleReEnableVideo = () => {
+    const tracks = currentStream.getVideoTracks();
+    if (tracks.length > 0) {
+      const track = tracks[0];
+      track.enabled = true;
+      setIsVideoMuted(false);
+    }
   };
 
   useEffect(() => {
@@ -76,6 +114,16 @@ function App() {
         <div>
           Video:
           <video id="localVideo" autoPlay playsInline ref={playerRef} style={{ transform: 'scaleX(-1)' }} />
+          <div>
+            {isMuted ?
+              <button onClick={handleUnMute}>Unmute</button> :
+              <button onClick={handleMute}>Mute</button>
+            }
+            {isVideoMuted ?
+              <button onClick={handleReEnableVideo}>Enable Video</button> :
+              <button onClick={handleDisableVideo}>Disable Video</button>
+            }
+          </div>
         </div>
       }
     </div>
